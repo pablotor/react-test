0a27f8199112aeeee5487c88218e41a882b140a7
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2020-08-11 12:08:11 -0300

    1er draft
     On branch master
     Changes to be committed:
    	modified:   package.json
    	modified:   src/App.css
    	modified:   src/App.js
    	new file:   src/App.lalala.js
    	new file:   src/hooks/input-hook.js

--- src/App.lalala.js
+++ src/App.lalala.js
@@ -3,13 +3,33 @@ import { useInput } from './hooks/input-hook.js';
 import logo from './logo.svg';
 import './App.css';
 
+function AppOld() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
 function App() {
   const { value, bind, reset } = useInput('');
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    alert(`Fetching posts from ${value}`);
-    getPosts(value);
+    alert(`Submitting Name ${value}`);
     reset();
   }
 
@@ -20,10 +40,8 @@ function App() {
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
-  const getPosts = (url) => {
-    setIsLoaded(false);
-    setError(null);
-    fetch(url)
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then(res => res.json())
       .then(
         (result) => {
@@ -38,43 +56,30 @@ function App() {
           setError(error);
         }
       )
-  }
-
-  let header = (
-      <header class="fb__1_2-header">
-        <div class="fb fb__2_5-title header-content">
-          <h1 id="header-title">Post Lister </h1>
-        </div>
-        <div class="fb fb__2_6-api-form header-content" id="submit-api" >
-
-          <form id="form" onSubmit={handleSubmit}>
-            <label>
-              Enter the URL with the posts in Json format:<br/>
-              <input id="form-bar" type="text" {...bind} />
-            </label>
-            <input id="input-button" type="submit" value="Submit" />
-          </form>
-        </div>
-      </header>
-  );
+  }, [])
 
   if (error) {
-    return (
-      <div>
-        {header}
-        <div>Error: {error.message}</div>
-      </div>
-    );
+    return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return (
-      <div>
-        {header}
-      </div>
-    );
+    return <div>Loading...</div>;
   } else {
     return (
       <div>
-        {header}
+        <header class="fb__1_2-header">
+          <div class="fb fb__2_5-title header-content">
+            <h1 id="header-title">Post Lister </h1>
+          </div>
+          <div class="fb fb__2_6-api-form header-content" id="submit-api" >
+
+            <form id="form" onSubmit={handleSubmit}>
+              <label>
+                Enter the URL with the posts in Json format:<br/>
+                <input id="form-bar" type="text" {...bind} />
+              </label>
+              <input id="input-button" type="submit" value="Submit" />
+            </form>
+          </div>
+        </header>
 
         <div class="fb fb__1_3-main" id="app" >
           {items.map(post => (
